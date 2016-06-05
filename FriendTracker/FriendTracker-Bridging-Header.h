917d103796cb3156dc603db0b3e924bff918c398
AaronGBoswell <Aaron@lakejoe.com>
2016-06-04 23:53:16 -0400

    added bridge

---
+++ FriendTracker/FriendTracker-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FriendTracker-Bridging-Header.h
+//  FriendTracker
+//
+//  Created by Aaron Boswell on 6/4/16.
+//  Copyright © 2016 Aaron Boswell. All rights reserved.
+//
+
+#ifndef FriendTracker_Bridging_Header_h
+#define FriendTracker_Bridging_Header_h
+
+
+#endif /* FriendTracker_Bridging_Header_h */
